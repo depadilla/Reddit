ca28f260b7ae075e7a20009cd501fd6455c8da37
Daniel Padilla <danedupadilla@gmail.com>
2019-02-26 11:05:29 -0600

    Restructured the comments on posts to work with posts stored in an array

--- Reddit.rb
+++ Reddit.rb
@@ -1,11 +1,14 @@
 require "HTTP"
-
+x = 0
 final_input = "no"
+system "clear"
 
 while final_input == "no" do 
-  print "Enter the name of a subreddit: "
-
-  # dogswithjobs
+  if x == 0
+    print "Enter the name of a subreddit: " 
+  elsif x > 0
+    print "Enter another subreddit to check-out: "
+  end
 
   sub_reddit = gets.chomp.to_s
 
@@ -28,45 +31,102 @@ while final_input == "no" do
     bullet += 1
   end
 
-  # puts "The first permalink is #{subreddit_info["data"]["children"][0]["data"]["permalink"]}"
-
   print "Choose post number: "
 
-  section = gets.chomp.to_i
+  @section = gets.chomp.to_i
 
-  comments = HTTP.get("https://www.reddit.com/r/#{sub_reddit}/comments/aou8i7/#{section}/.json")
+  # puts "The permalink chosen #{subreddit_info["data"]["children"][@section - 1]["data"]["permalink"]}"   # Sanity check for correct permalink
 
-  comments_info = comments.parse
+  link_to_permalink = subreddit_info["data"]["children"][@section - 1]["data"]["permalink"]
+
+  comment_web_address = "https://www.reddit.com"+link_to_permalink+".json".to_s
+
+  comment_web = HTTP.get("#{comment_web_address}")
+
+  comments_info = comment_web.parse
 
   all_comments = comments_info[1]["data"]["children"].length
 
-
+  puts " "
+  puts "----- Here are the commments for '#{posts_array[@section]}' -----"  
   index2 = 0
   bullet2 = 1
-  all_comments.times do               #user still has to copy/paste the post they want to open, will need to change so the user inputs the number instead of the whole title.
+  all_comments.times do             
     puts " "
     one_comment = comments_info[1]["data"]["children"][index2]["data"]["body"]
-    puts "#{bullet2}) " + one_comment
+    puts "#{bullet2}) "+one_comment.to_s
     index2 += 1
     bullet2 += 1
   end
 
-
+  puts " "
+  puts "------------------------------------------"  
   print "Do you want to exit? Type 'yes' or 'no': "
   @final_input = gets.chomp.to_s
 
   if @final_input == "yes"
     system "clear"
+    break
   else
     system "clear"
+    x += 1
   end
 end
 
 
 
 
-# def get_comment
-#   comment = comments_info[1]["data"]["children"][index2]["data"]["permalink"]
+def get_post (sub_reddit)
+  sub_reddit = gets.chomp.to_s
+
+  response = HTTP.get("https://www.reddit.com/r/#{sub_reddit}/.json")
 
 
-# end
+  subreddit_info = response.parse
+  posts = subreddit_info["data"]["children"].length
+
+  index = 0
+  bullet = 1
+  posts_array = ["empty", ]
+  posts.times do
+    puts " "
+    posts_array << subreddit_info["data"]["children"][index]["data"]["title"].to_s          # Adds all the posts into the array.
+    puts "#{bullet}) " + posts_array[bullet].to_s
+    puts " "
+
+    index += 1
+    bullet += 1
+  end
+end
+
+
+
+
+def get_comment (*)
+
+  print "Choose post number: "
+
+  @section = gets.chomp.to_i
+
+  link_to_permalink = subreddit_info["data"]["children"][@section - 1]["data"]["permalink"]
+
+  comment_web_address = "https://www.reddit.com"+link_to_permalink+".json".to_s
+
+  comment_web = HTTP.get("#{comment_web_address}")
+
+  comments_info = comment_web.parse
+
+  all_comments = comments_info[1]["data"]["children"].length
+
+  puts " "
+  puts "----- Here are the commments for '#{posts_array[@section]}' -----"  
+  index2 = 0
+  bullet2 = 1
+  all_comments.times do             
+    puts " "
+    one_comment = comments_info[1]["data"]["children"][index2]["data"]["body"]
+    puts "#{bullet2}) "+one_comment.to_s
+    index2 += 1
+    bullet2 += 1
+  end
+end
